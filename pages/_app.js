17c23b86b6502776dfbd652c57bb82a721a855cf
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-05-07 11:36:54 +0100

    add SpeedInsight package

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
