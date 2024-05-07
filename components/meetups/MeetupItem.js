20e94ffa168b927f7bf6523dd5d5dacffd58c660
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2024-05-07 10:31:29 +0100

    replace img element with next/Images Library

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import Image from 'next/image';
 
 function MeetupItem(props) {
   const router = useRouter()
@@ -12,7 +13,7 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <Image src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
